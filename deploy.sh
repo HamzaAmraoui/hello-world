d2732f0adef89712bb487ec29870b4176a2afca8
Hamza Amraoui <hamzafuego2@gmail.com>
2025-04-03 12:48:32 +0200

    PRUEBA GITHUB ACTION TOKEN

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 env=$1
-DB_USER=$2
-DB_PASS=$3
-API_KEY=$4
-
-if [ -z "$env" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASS" ] || [ -z "$API_KEY" ]; then
-  echo " Error: Faltan argumentos. Uso: ./deploy.sh [ENV] [DB_USER] [DB_PASS] [API_KEY]"
-  exit 1
-fi
 
 hackaton deploy --$env
 hackaton restart app --$env
