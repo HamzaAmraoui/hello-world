25c10b4dc876c5fc61b7e250a13b54dd00d98933
Hamza Amraoui <hamzafuego2@gmail.com>
2025-04-24 09:38:14 +0200

    hfgh

--- src/utilidades/Versionador.groovy
+++ src/utilidades/Versionador.groovy
@@ -2,6 +2,9 @@ package utilidades
 
 class Versionador implements Serializable {
     static String generar(String branch, String commit) {
+        if (!branch || !commit) {
+            return "unknown-unknown"
+        }
         return "${branch}-${commit.take(7)}"
     }
 }
